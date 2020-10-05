b4cc75defd9ea373dd8a2b21828a99231bc9d169
Srinivas Tamada <srinivas@9lessons.info>
2020-10-05 16:49:07 -0400

    a

--- src/app/service/translate-config.service.ts
+++ src/app/service/translate-config.service.ts
@@ -6,7 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TranslateConfigService {
   constructor(private translateService: TranslateService) {
-    this.translateService.use('fr');
+    this.translateService.use('en');
   }
 
   changeLanguage(type: string){
